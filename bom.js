7782dd7d64de1a7630ae62d037b673c8a7d3e7db
hinunk <526996400@qq.com>
2017-04-25 17:14:34 +0800

    first

---
+++ bom.js
@@ -0,0 +1,11 @@
+window.$ = function () {
+  
+}
+
+$.bom = {
+	openAtCenter: function(width,height,url) {
+		window.open(url,'_blank',`width=${width}px,height=${height}px,
+		screenX=${screen.width/2 - width/2}px,screenY=${screen.height/2 - height/2}px`)
+	}
+}
+ 
