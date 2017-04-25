a1d1f926681fb2951b244179f86e4a49f79298f1
hinunk <526996400@qq.com>
2017-04-26 03:19:10 +0800

    修改

--- bom.js
+++ bom.js
@@ -6,13 +6,19 @@ window.$ = function () {
 $.bom = {
   openAtCenter: function (width, height, url) {
     window.open(url, '_blank', `width=${width}px,height=${height}px,
-    screenX=${screen.width / 2 - width / 2}px,screenY=${screen.height / 2 - width / 2}`)
+    screenX=${screen.width / 2 - width / 2}px,screenY=${screen.height / 2 - width / 2}px`)
   },
-
-}
-
-$.bom.search =  function (name, value) {
-  if (value === undefined) {
-    return window.location.search
+  search: function (name, value) {
+    if (value === undefined) {
+      let search = window.location.search
+      if(search[0] === "?"){
+      	search = search.slice(1)
+      }
+      let searchArray = search.split('&')
+			for(var i = 0;i<searchArray.length;i++){
+				console.log(searchArray[i])
+			}
+      return window.location.search
+    }
   }
 }
