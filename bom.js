4b5562b1d745305415305e0c092410e24f78bbaf
hinunk <526996400@qq.com>
2017-04-26 00:32:55 +0800

    ls

--- bom.js
+++ bom.js
@@ -1,11 +1,18 @@
 window.$ = function () {
-  
+  var array = []
+  return array
 }
 
 $.bom = {
-	openAtCenter: function(width,height,url) {
-		window.open(url,'_blank',`width=${width}px,height=${height}px,
-		screenX=${screen.width/2 - width/2}px,screenY=${screen.height/2 - height/2}px`)
-	}
+  openAtCenter: function (width, height, url) {
+    window.open(url, '_blank', `width=${width}px,height=${height}px,
+    screenX=${screen.width / 2 - width / 2}px,screenY=${screen.height / 2 - width / 2}`)
+  },
+
 }
- 
+
+$.bom.search =  function (name, value) {
+  if (value === undefined) {
+    return window.location.search
+  }
+}
