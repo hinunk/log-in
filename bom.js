a590dd081aeb2fff923e68b774e9126a40aa5814
hinunk <526996400@qq.com>
2017-04-26 18:14:53 +0800

    2

--- bom.js
+++ bom.js
@@ -1,26 +1,41 @@
-window.$ = function () {
-  var array = []
-  return array
+window.$ = function() {
+	var array = []
+	return array
 }
 
 $.bom = {
-  openAtCenter: function (width, height, url) {
-    window.open(url, '_blank', `width=${width}px,height=${height}px,
+	openAtCenter: function(width, height, url) {
+		window.open(url, '_blank', `width=${width}px,height=${height}px,
     screenX=${screen.width / 2 - width / 2}px,screenY=${screen.height / 2 - width / 2}px`)
-  },
-  search: function (name, value) {
-  	let result = {}
-    if (value === undefined) {
-      let search = window.location.search
-      if(search[0] === "?"){
-      	search = search.slice(1)
-      }
-      let searchArray = search.split('&')
-			for(var i = 0;i<searchArray.length;i++){
-				let am = searchArray[i].split('=')
-				result[am[0]] = am[1]
+	},
+	search: function(name, value) {
+		let searchAll = function() {
+			let result = {}
+			let search = window.location.search
+			if(search[0] === '?') {
+				search = search.slice(1)
 			}
-      return decodeURIComponent(result[name])
-    }
-  }
-}   
+			let searchArray = search.split('&')
+			for(var i = 0; i < searchArray.length; i++) {
+				let parts = searchArray[i].split('=')
+				result[decodeURIComponent(parts[0])] = decodeURIComponent(parts[1] || '')
+			}
+			return result
+		}
+		let result = searchAll()
+		if(value === undefined) {
+			return result[name]
+		} else {
+			if(result[name] === undefined) {
+				location.search += `&${encodeURIComponent(name)}=${encodeURIComponent(value)}`
+			} else {
+				result[name] = encodeURIComponent(value)
+				let newSearch = '?'
+				for(let key in result) {
+					newSearch += `${encodeURIComponent(key)}=${encodeURIComponent(result[key])}&`
+				}
+				location.search = newSearch
+			}
+		}
+	}
+}
